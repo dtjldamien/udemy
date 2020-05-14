5248fe533ab28bad66b14e98e8ba7ca182ae4644
Damien Tan <dtjldamien@gmail.com>
2020-05-14 18:07:52 +0800

    ignroe dev.js

--- mern/daygram/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://dtjldamien:o6JoesgZtgVy2j1q@udemy-mern-96ooo.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "udemy_mern",
-    SEND_GRID: "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"
-}
