7e3a48c799f2d254214d26b87b6df619ef1ab90d
Damien Tan <dtjldamien@gmail.com>
2020-06-04 20:55:27 +0800

    trying next.js

---
+++ nextjs/pages/about.js
@@ -0,0 +1,12 @@
+// about page
+import Navbar from '../components/Navbar';
+
+const About = () => (
+    <div>
+        <Navbar />
+        <h1>About Next.js</h1>
+        <p>Application to view BitCoin Prices</p>
+    </div>
+);
+
+export default About;
---
+++ nextjs/pages/index.js
@@ -0,0 +1,11 @@
+// root/index page
+import Navbar from '../components/Navbar';
+
+const Index = () => (
+    <div>
+        <Navbar />
+        <h1>Welcome to Next.js</h1>
+    </div>
+);
+
+export default Index;
